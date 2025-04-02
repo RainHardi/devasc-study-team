7c5cb565e24aa1a5d9bb575fea7d8c0e75d6e28c
elee02 <elyorrasulov1822@gmail.com>
2025-04-02 23:46:05 +0900

    Add YAML output for JSON data and fix newline in refresh token message

--- parsejson.py
+++ parsejson.py
@@ -10,3 +10,6 @@ print("The access token is: {}".format(ourjson['access_token']))
 print("The token expires in {} seconds.".format(ourjson['expires_in']))
 print("The refresh token is: {}".format(ourjson['refresh_token']))
 print("The refresh token expires in {} seconds.".format(ourjson['refreshtokenexpires_in']))
+
+print("\n\n---")
+print(yaml.dump(ourjson))
